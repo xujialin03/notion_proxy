cd0a355e99c04363c967021b913c2915d4f3f3a9
xujialin03 <lin_7788@yeah.net>
2022-03-05 22:52:36 +0800

    1111

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express=require('express');
 const port=process.env.PORT || 3000;
 var app=express();
 app.get('/',function(req,res){
+    res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});//设置response编码为utf-8
     res.write("网站名:" + req.query.name);
     res.write("\n");
     res.write("网站 URL:" + req.query.url);
