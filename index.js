cf000c4a6ee06f92f42b666acd466096f6c11ec7
xujialin03 <lin_7788@yeah.net>
2022-03-05 22:04:20 +0800

    1111

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var express=require('express');
 const port=process.env.PORT || 3000;
 var app=express();
 app.get('/',function(req,res){
-    res.send("hello world 部署完成");
+    res.send("hello world 部署完成1");
 });
 app.listen(port);
