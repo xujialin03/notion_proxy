eb077c1cc106ffc9f369fdb75b99ca1acd6d5375
xujialin03 <lin_7788@yeah.net>
2022-03-09 13:13:51 +0800

    更新时区

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 var express=require('express');
 const https = require('https');
 const port=process.env.PORT || 3000;
+process.env.TZ = 'Asia/Shanghai';
 var app=express();
-process.env.TZ = 'Asia/Shanghai'
+
 const project_database_id='84b35e6f-6d2e-49d3-a173-92439a4621b5';
 const todo_database_id="52967d09-6868-4438-ac83-070fb8f3b288";
 const learn_database_id="58fe3237-c47c-4763-9770-58797c178e33";
